ec7e7dd43b96fd35686d71ba0a8ec61c25fd693c
RL-007 <rl-007@RL-007s-iMac-2.local>
2015-05-16 22:09:12 +0800

    adds type field

--- OOTD/main/forms.py
+++ OOTD/main/forms.py
@@ -2,4 +2,22 @@ from django import forms
 
 class UploadForm(forms.Form):
 	picture = forms.FileField()
-	sex = forms.ChoiceField(widget=forms.RadioSelect, choices=(('1', 'Men'), ('2', 'Women')))
+	women_choices = (
+		('1', 'accessories'),
+		('2', 'coats/sweater'),
+		('3', 'dresses'),
+		('4', 'pants'),
+		('5', 'shorts'),
+		('6', 'skirts'),
+		('7', 'tops')
+	)
+	men_choices = (
+		('1', 'accessories'),
+		('2', 'pants'),
+		('3', 'shirts'),
+		('4', 'shorts'),
+		('5', 'suits')
+	)
+	
+	sex = forms.ChoiceField(widget=forms.RadioSelect, choices=(('1', 'Men'), ('2', 'women')))
+	clothe_type = forms.ChoiceField(choices=(women_choices + men_choices))
